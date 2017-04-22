c40cd8a445f16183ecbb427e6333c3c7aa4d2d18
aditya0111 <dhanrajaditya01@gmail.com>
2017-04-22 16:57:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,334 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>FoodCubo || Home</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+<!--Animation-->
+<script src="js/wow.min.js"></script>
+<link href="css/animate.css" rel='stylesheet' type='text/css' />
+<script>
+	new WOW().init();
+</script>
+<script src="js/simpleCart.min.js"> </script>	
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+				});
+			});
+</script>
+</head>
+<body>
+<script src='../../../../ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script>
+</script>
+
+
+<body>
+    
+	<?php include_once 'includes/header.php'; ?>
+	<!-- content-section-starts -->
+	<div class="content">
+		<div class="ordering-section" id="Order">
+			<div class="container">
+				<div class="ordering-section-head text-center wow bounceInRight" data-wow-delay="0.4s">
+					<h3>Order Food at your Finger Tips</h3>
+					<div class="dotted-line">
+						<h4>Just 4 steps to follow </h4>
+					</div>
+				</div>
+				<div class="ordering-section-grids">
+					<div class="col-md-3 ordering-section-grid">
+						<div class="ordering-section-grid-process wow fadeInRight" data-wow-delay="0.4s"">
+							<i class="one"></i><br>
+							<i class="one-icon"></i>
+							<p>Choose <span>Your Delicacy</span></p>
+							<label></label>
+						</div>
+					</div>
+					<div class="col-md-3 ordering-section-grid">
+						<div class="ordering-section-grid-process wow fadeInRight" data-wow-delay="0.4s"">
+							<i class="two"></i><br>
+							<i class="two-icon"></i>
+							<p>Choose <span>Restaurant</span></p>
+							<label></label>
+						</div>
+					</div>
+					<div class="col-md-3 ordering-section-grid">
+						<div class="ordering-section-grid-process wow fadeInRight" data-wow-delay="0.4s"">
+							<i class="three"></i><br>
+							<i class="three-icon"></i>
+							<p><span>Pay On Delievery</span> OR<span> Online</span></p>
+							<label></label>
+						</div>
+					</div>
+					<div class="col-md-3 ordering-section-grid">
+						<div class="ordering-section-grid-process wow fadeInRight" data-wow-delay="0.4s"">
+							<i class="four"></i><br>
+							<i class="four-icon"></i>
+							<p>Enjoy <span>your food </span></p>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div class="special-offers-section">
+			<div class="container">
+				<div class="special-offers-section-head text-center dotted-line">
+					<h4>Special Offers</h4>
+				</div>
+				<div class="special-offers-section-grids">
+				 <div class="m_3"><span class="middle-dotted-line"> </span> </div>
+				   <div class="container">
+					  <ul id="flexiselDemo3">
+						<li>
+							<div class="offer">
+								<div class="offer-image">	
+									<img src="images/moose.jpg" class="img-responsive" alt=""/>
+								</div>
+								<div class="offer-text">
+									<h4>Mosse Cake Combo of 3</h4>
+									<p>It's delicious grab it. </p>
+									<input type="button" value="Grab It">
+									<span></span>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</li>
+						<li>
+							<div class="offer">
+								<div class="offer-image">	
+									<img src="images/chicken_lolipop.jpg" class="img-responsive" alt=""/>
+								</div>
+								<div class="offer-text">
+									<h4>Chicken Jumbo</h4>
+									<p>It's delicious grab it. </p>
+									<input type="button" value="Grab It">
+									<span></span>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</li>
+						<li>
+							<div class="offer">
+								<div class="offer-image">	
+									<img src="images/lollipop.jpg" class="img-responsive" alt=""/>
+								</div>
+								<div class="offer-text">
+									<h4>Chees Cake Lollipop Combo</h4>
+									<p>It's delicious grab it </p>
+									<input type="button" value="Grab It">
+									<span></span>
+								</div>
+								
+								<div class="clearfix"></div>
+								</div>
+						</li>
+						<li>
+							<div class="offer">
+								<div class="offer-image">	
+									<img src="images/moose.jpg" class="img-responsive" alt=""/>
+								</div>
+								<div class="offer-text">
+									<h4>Mosse Cake Combo of 4</h4>
+									<p>It's delicious grab it. </p>
+									<input type="button" value="Grab It">
+									<span></span>
+								</div>
+								
+								<div class="clearfix"></div>
+								</div>
+					    </li>
+					 </ul>
+				 <script type="text/javascript">
+					$(window).load(function() {
+						
+						$("#flexiselDemo3").flexisel({
+							visibleItems: 3,
+							animationSpeed: 1000,
+							autoPlay: false,
+							autoPlaySpeed: 3000,    		
+							pauseOnHover: true,
+							enableResponsiveBreakpoints: true,
+							responsiveBreakpoints: { 
+								portrait: { 
+									changePoint:480,
+									visibleItems: 1
+								}, 
+								landscape: { 
+									changePoint:640,
+									visibleItems: 2
+								},
+								tablet: { 
+									changePoint:768,
+									visibleItems: 3
+								}
+							}
+						});
+						
+					});
+				    </script>
+				    <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+				</div>
+			</div>
+		</div>
+		</div>
+		
+		<div class="popular-restaurents" id="Popular-Restaurants">
+			<div class="container">
+				<div class="col-md-4 top-restaurents">
+					<div class="top-restaurent-head">
+						<h3>Top Restaurants</h3>
+					</div>
+					<div class="top-restaurent-grids">
+						<div class="top-restaurent-logos">
+							<div class="res-img-1 wow bounceIn" data-wow-delay="0.4s">
+								<img src="images/restaurent-1.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="res-img-2 wow bounceIn" data-wow-delay="0.4s">
+							    <img src="images/restaurent-2.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="res-img-1 wow bounceIn" data-wow-delay="0.4s">
+							    <img src="images/restaurent-3.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="res-img-2 wow bounceIn" data-wow-delay="0.4s">
+							    <img src="images/restaurent-4.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="res-img-1 nth-grid1 wow bounceIn" data-wow-delay="0.4s">
+							    <img src="images/restaurent-5.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="res-img-2 nth-grid1 wow bounceIn" data-wow-delay="0.4s">
+							    <img src="images/restaurent-6.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-8 top-cuisines">
+					<div class="top-cuisine-head">
+						<h3>Top Cuisines</h3>
+					</div>
+					<div class="top-cuisine-grids">
+						<div class="top-cuisine-grid wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/0" class="img-responsive" alt="" /> </a>
+							<label>Paneer Tikka</label>
+					    </div>
+						<div class="top-cuisine-grid wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/2.jpg" class="img-responsive" alt="" /> </a>
+							<label>Burger</label>
+					    </div>
+						<div class="top-cuisine-grid wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/8.jpg" class="img-responsive" alt="" /> </a>
+							<label>Chicken Lollipop</label>
+					    </div>
+						<div class="top-cuisine-grid nth-grid wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/3.jpg" class="img-responsive" alt="" /> </a>
+							<label>Daal Makhani</label>
+					    </div>
+						<div class="top-cuisine-grid nth-grid1 wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/4.jpg" class="img-responsive" alt="" /> </a>
+							<label>Moose Cake</label>
+					    </div>
+						<div class="top-cuisine-grid nth-grid1 wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/5.jpg" class="img-responsive" alt="" /> </a>
+							<label>Chillie Baby Corn</label>
+					    </div>
+						<div class="top-cuisine-grid nth-grid1 wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/6.jpg" class="img-responsive" alt="" /> </a>
+							<label>Chesse Chocolate Cake</label>
+					    </div>
+						<div class="top-cuisine-grid nth-grid nth-grid1 wow bounceIn" data-wow-delay="0.4s">
+						    <a href="#"><img src="images/7.jpg" class="img-responsive" alt="" /> </a>
+							<label>Gulab Jamun</label>
+					    </div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="service-section">
+			<div class="service-section-top-row">
+				<div class="container">
+					<div class="service-section-top-row-grids wow bounceInLeft" data-wow-delay="0.4s">
+					<div class="col-md-3 service-section-top-row-grid1">
+						<h3>Enjoy Exclusive Food Order any of these</h3>
+					</div>
+					<div class="col-md-2 service-section-top-row-grid2">
+						<ul>
+							<li><i class="arrow"></i></li>
+							<li class="lists">Party Orders</li>
+						</ul>
+						<ul>
+							<li><i class="arrow"></i></li>
+							<li class="lists">Home Made Food</li>
+						</ul>
+						<ul>
+							<li><i class="arrow"></i></li>
+							<li class="lists"> Diet Food </li>
+						</ul>
+					</div>
+					<div class="col-md-5 service-section-top-row-grid3">
+						<img src="images/lunch.png" class="img-responsive" alt="" />
+					</div>
+					<div class="col-md-2 service-section-top-row-grid4 wow swing animated" data-wow-delay= "0.4s">
+						<a href="order.php"><input type="submit" value="Order Now"></a>
+					</div>
+					<div class="clearfix"></div>
+					</div>
+				</div>
+			</div>
+			<div class="service-section-bottom-row">
+				<div class="container">
+					<div class="col-md-4 service-section-bottom-grid wow bounceIn "data-wow-delay="0.2s">
+						<div class="icon">
+							<img src="images/icon1.jpg" class="img-responsive" alt="" />
+						</div>
+						<div class="icon-data">
+							<h4>100% Service Guarantee</h4>
+							<p>Guranteed service assistance and food delievery at your door steps. </p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="col-md-4 service-section-bottom-grid wow bounceIn "data-wow-delay="0.2s">
+						<div class="icon">
+							<img src="images/icon2.jpg" class="img-responsive" alt="" />
+						</div>
+						<div class="icon-data">
+							<h4>Fully Secure Payment</h4>
+							<p>Do secure payments with our various payment methods including C.O.D.  </p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="col-md-4 service-section-bottom-grid wow bounceIn "data-wow-delay="0.2s">
+						<div class="icon">
+							<img src="images/icon3.jpg" class="img-responsive" alt="" />
+						</div>
+						<div class="icon-data">
+							<h4>Quick Food Near You</h4>
+							<p>One click ordering from the food joints near you.</p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<?php include_once 'includes/footer.php'; ?>
+
+</body>
+</html>
