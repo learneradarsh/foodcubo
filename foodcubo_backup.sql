1571bd5ac96273af7a10cfe7e1278bd407fc5742
Adarsh Learner <learner.adarsh@gmail.com>
2017-11-26 18:55:37 +0530

    changes done in db file

--- foodcubo_backup.sql
+++ foodcubo_backup.sql
@@ -49,7 +49,6 @@ DROP TABLE IF EXISTS `contact_us`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contact_us` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
   `subject` varchar(500) NOT NULL,
